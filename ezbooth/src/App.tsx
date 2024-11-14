da593ea220676531824c200d409d66ef8572d862
오주현 <dhwngus99@gmail.com>
2024-11-14 15:13:28 +0900

    ✨ feat(font setup): setting font-family: pretendard(prototype)

--- ezbooth/src/App.tsx
+++ ezbooth/src/App.tsx
@@ -1,13 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import { Routes, Route } from 'react-router-dom';
-import Test from 'components/Test';
+import Home from 'pages/Home';
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Test/>}></Route>
+      <Route path="/" element={<Home/>}></Route>
     </Routes>
   );
 }
---
+++ ezbooth/src/pages/Home.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home = () => {
+
+  return (
+    <div className='Home' style={{fontWeight: 500, fontSize:'100px'}}>
+      home의 기능을 무료 체험 해보세요.
+    </div>
+  )
+};
+
+export default Home;
